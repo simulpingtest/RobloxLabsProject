5d28900169043c31938520a7c311c12ce8dca55f
mylespepper2737372737 <86173222+mylespepper2737372737@users.noreply.github.com>
2021-09-07 14:43:17 -0400

    Update Update.js

--- js/Animations/Update.js
+++ js/Animations/Update.js
@@ -1,64 +1,86 @@
-/*
+if (typeof Roblox === "undefined") {
+    Roblox = {};
+}
+if (typeof Roblox.Animations === "undefined") {
+    Roblox.Animations = {};
+}
+Roblox.Animations.Update = function () {
 
-This JS Class reads device meta from meta tag and provides DeviceMeta
-object which carries information about device type the user is on.
+    var nameErrorText = "";
 
-Author: Achint Verma
-Date: Aug 10, 2017
+    var initializeStrings = function () {
+        nameErrorText = Roblox.Animations.Update.Resources.nameError;
+    };
 
-*/
-
-var Roblox = Roblox || {};
-Roblox.DeviceMeta = (function () {
-
-    // read meta information from tag
-    var metaTag = document.querySelector('meta[name="device-meta"]');
-    if (metaTag === null) {
-        console.debug("Error loading device information from meta tag - please check if meta tag is present");
-        return;
+    function showNameError() {
+        $('#nameRow span').removeClass("field-validation-valid");
+        $('#nameRow span').addClass("field-validation-error");
+        $('#nameRow span').text(nameErrorText);
     }
-    var keyMap = metaTag.dataset || {};
 
-    // possible app types 
-    var appTypes = {
-        android: "android",
-        ios: "ios",
-        xbox: "xbox",
-        uwp: "uwp",
-        amazon: "amazon",
-        win32: "win32",
-        universalapp: "universalApp",
-        unknown: "unknown"
+    return {
+        initializeStrings: initializeStrings,
+        showNameError: showNameError
     };
+} ();
 
-    // possible device types
-    var deviceTypes = {
-        computer: "computer",
-        tablet: "tablet",
-        phone: "phone",
-        console: "console"
-    };
-    
-    // this constructor mechanism prevents overwriting flags by returning a new copy everytime
-    return function () {
-        return {
-            deviceType: deviceTypes[keyMap.deviceType] || '',
-            appType: appTypes[keyMap.appType] || '',
-            isInApp: keyMap.isInApp === 'true',
-            isDesktop: keyMap.isDesktop === 'true',
-            isPhone: keyMap.isPhone === 'true',
-            isTablet: keyMap.isTablet === 'true',
-            isConsole: keyMap.isConsole === 'true',
-            isAndroidApp: keyMap.isAndroidApp === 'true',
-            isIosApp: keyMap.isIosApp === 'true',
-            isUWPApp: keyMap.isUwpApp === 'true',
-            isXboxApp: keyMap.isXboxApp === 'true',
-            isAmazonApp: keyMap.isAmazonApp === 'true',
-            isWin32App: keyMap.isWin32App === 'true',
-            isStudio: keyMap.isStudio === 'true',
-            isIosDevice: keyMap.isIosDevice === 'true',
-            isAndroidDevice: keyMap.isAndroidDevice === 'true',
-            isUniversalApp: keyMap.isUniversalApp === 'true'
+$(function () {
+
+    function showOrHideNameError() {
+        if ($("input#Name").val().trim() == '') {
+            Roblox.Animations.Update.showNameError();
+            Roblox.StyleguideConversions.convertMvcErrorToStyleGuide();
+        } else {
+            $('#nameRow span').html("").removeClass("tool-tip");
         }
+    }
+
+    $("input#Name").bind('blur keyup', function () {
+        showOrHideNameError();
+    });
+
+    $("#okButton").click(function () {
+        $('form#animationForm').submit();
+        $(this).addClass('btn-disabled-primary');
+        $(this).prop("disabled", true);
+        showProcessingModal();
+        return false;
+    });
+
+    $('#cancelButton').click(function () {
+        document.location = $(this).attr('href');
+    });
+
+    function switchTabs(nextTabElem) {
+        var currentTab = $('div.verticaltab.selected');
+        currentTab.removeClass('selected');
+        $('#' + currentTab.data('id')).hide();
+        nextTabElem.addClass('selected');
+        $('#' + nextTabElem.data('id')).show();
+        $('#navbar').removeClass(currentTab.attr('id'));
+        $('#navbar').addClass(nextTabElem.attr('id'));
+    }
+
+    $('div.verticaltab').bind('click', function () {
+        switchTabs($(this));
+    });
+
+    showOrHideNameError();
+    Roblox.StyleguideConversions.convertMvcErrorToStyleGuide();
+    $('#navbar').addClass($('div.verticaltab.selected').attr('id'));
+
+    function showProcessingModal(closeClass) {
+        var modalProperties = { overlayClose: false, opacity: 80, overlayCss: { backgroundColor: "#000" }, escClose: false };
+
+        if (typeof closeClass !== "undefined" && closeClass !== "") {
+            $.modal.close("." + closeClass);
+        }
+
+        $("#ProcessingView").modal(modalProperties);
     };
-})();
+
+    //go to the Basic Settings Tab if there was an error in form submission
+    if ($('div.validation-summary-errors').attr('data-valmsg-summary') == 'true') {
+        switchTabs($('#basicSettingsTab'));
+    }
+});
